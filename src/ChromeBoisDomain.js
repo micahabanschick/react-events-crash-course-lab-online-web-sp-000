e7a26692f38351563d126b29f9683fdb95f7e747
Micah Banschick <micha.ban@gmail.com>
2021-03-23 00:11:21 +0000

    Automatically backed up by Learn

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -39,6 +39,8 @@ export default class ChromeBoisDomain extends Component {
     return (
       <canvas
         onMouseMove={this.handleMouseMove}
+        onClick={() => { toggleCycling() }}
+        onKeyDown={this.changeSize}
         width='900'
         height='600'
         tabIndex="0">
